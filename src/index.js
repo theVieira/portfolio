e5f212e9f361feedb9a7f9042bee77e0b24366e6
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-09-19 11:00:23 -0400

    feat: add particles.js to background animation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import { show, hide, link } from "./animation/menu.js";
-import information from "./animation/information.js";
-import observer from "./animation/scroll.js";
-import copy from "./copy.js";
+import { show, hide, link } from './animation/menu.js'
+import information from './animation/information.js'
+import observer from './animation/scroll.js'
+import copy from './copy.js'
+
+particlesJS.load('particles-js', '../assets/particles.json')
